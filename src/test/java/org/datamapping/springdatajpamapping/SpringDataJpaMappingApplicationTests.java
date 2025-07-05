9e157208b30fd38ac196d1afa21bc3856b450d19
aks <dhpdgwmqo98oafdo@gmail.com>
2025-07-05 20:39:06 +0530

    initial commit

---
+++ src/test/java/org/datamapping/springdatajpamapping/SpringDataJpaMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.datamapping.springdatajpamapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
